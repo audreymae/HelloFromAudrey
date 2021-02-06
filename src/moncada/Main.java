19c277b60a4cb523fa17767b9c95ab56e94d8565
Clara Chan <cha19018@byui.edu>
2021-02-07 04:31:37 +0800

    Committing Clara's changes to audreys cs 246 prove 04 assignment

--- src/moncada/Main.java
+++ src/moncada/Main.java
@@ -3,6 +3,9 @@ package moncada;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Audrey Mae Moncada");
+
+        System.out.println("Hello, my name is Audrey Mae Moncada");
+        System.out.println("Hello Audrey, my name is Clara, your team mate.");
+
     }
 }
